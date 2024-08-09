d05ff7c4c12f6cec86a78a3cff2cdfead715c944
neiljaywarner <njwandroid@gmail.com>
2024-08-09 17:04:25 -0500

    made a video from this

--- lib/src/features/onboarding/presentation/onboarding_screen.dart
+++ lib/src/features/onboarding/presentation/onboarding_screen.dart
@@ -1,54 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:go_router/go_router.dart';
-import 'package:starter_architecture_flutter_firebase/src/common_widgets/primary_button.dart';
 import 'package:starter_architecture_flutter_firebase/src/common_widgets/responsive_center.dart';
 import 'package:starter_architecture_flutter_firebase/src/constants/app_sizes.dart';
-import 'package:starter_architecture_flutter_firebase/src/features/onboarding/presentation/onboarding_controller.dart';
-import 'package:starter_architecture_flutter_firebase/src/localization/string_hardcoded.dart';
-import 'package:starter_architecture_flutter_firebase/src/routing/app_router.dart';
 
 class OnboardingScreen extends ConsumerWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.watch(onboardingControllerProvider);
-    return Scaffold(
+    return const Scaffold(
       body: ResponsiveCenter(
         maxContentWidth: 450,
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Text(
-              'Do we need an onboarding screen\n If we did, we would probably want an image with it',
-              style: Theme.of(context).textTheme.headlineSmall,
+              'The "Test for Video 9-aug" campaign \ntriggers the above dynamic banner\nA non-dev  can with web UI modify color, image '
+              '\n and the url it goes to from the eb'
+              '\nIf your app supports deep links you can use those'
+              '\n(although it must be https.)'
+              '\n The in app messaging can at most once a day\n it can be dependent on events or audiences.\n '
+              '\n Modal, Image only and cards are also supported.'
+              '\n as well as two buttons for actions and some automatic analytics.',
               textAlign: TextAlign.center,
             ),
             gapH16,
-            SvgPicture.asset(
-              'assets/time-tracking.svg',
-              width: 200,
-              height: 200,
-              semanticsLabel: 'Time tracking logo',
-            ),
-            gapH16,
-            PrimaryButton(
-              text: 'Get Started'.hardcoded,
-              isLoading: state.isLoading,
-              onPressed: state.isLoading
-                  ? null
-                  : () async {
-                      await ref.read(onboardingControllerProvider.notifier).completeOnboarding();
-                      if (context.mounted) {
-                        // go to sign in page after completing onboarding
-                        context.goNamed(AppRoute.signIn.name);
-                      }
-                    },
-            ),
           ],
         ),
       ),
